78e7a83de3e12d6c6735ea8f872d510434371e5a
LeslieTan <leslietan0521@gmail.com>
2025-09-30 16:21:24 +0800

    1

--- TeamCode/src/main/java/pedroPathing/Constants.java
+++ TeamCode/src/main/java/pedroPathing/Constants.java
@@ -3,8 +3,14 @@ package pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
@@ -14,6 +20,30 @@ public class Constants {
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                /* other builder steps */
                 .build();
     }
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFrontMotor")
+            .rightRearMotorName("rightBackMotor")
+            .leftRearMotorName("leftBackMotor")
+            .leftFrontMotorName("leftFrontMotor")
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+    //不确定odo是不是这个"goBILDA Pinpoint Odometry Computer"
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-5) //这个要改，应该就是之前测过的odo的offsetx和offsety，单位是inch
+            .strafePodX(0.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("odo")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+
+
 }
