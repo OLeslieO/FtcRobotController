cd93fc90c2331e80fe1fcd5141ef7b3e8a73d1dd
LeslieTan <leslietan0521@gmail.com>
2025-10-01 16:19:08 +0800

    尝试配置panel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -9,6 +9,7 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+
 import pedroPathing.Constants;
 
 @Autonomous(name = "Example Auto", group = "Examples")
