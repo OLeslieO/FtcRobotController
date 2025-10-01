056528263cb7d0e4f0f7567855a7e53cb0297606
LeslieTan <leslietan0521@gmail.com>
2025-10-01 14:52:30 +0800

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -18,19 +18,25 @@ public class AutoDrive extends OpMode {
     private Timer pathTimer, actionTimer, opmodeTimer;
 
     private int pathState;
-    private final Pose startPose = new Pose(28.5, 128, Math.toRadians(180)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(28.5, 50, Math.toRadians(180)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private final Pose startPose = new Pose(72, 72, Math.toRadians(90)); // Start Pose of our robot.
+    private final Pose scorePose = new Pose(28.5, 50, Math.toRadians(180));// Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private final Pose scanbluePose = new Pose(27,120,Math.toRadians(-40));
+    private final Pose scanredPose = new Pose(115,120,Math.toRadians(-140));
     private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
     private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
     private final Pose pickup3Pose = new Pose(49, 135, Math.toRadians(0)); // Lowest (Third Set) of Artifacts from the Spike Mark.
 
     private Path scorePreload;
-    private PathChain grabPickup1, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3;
+    private Path sacnPreload;
+    private PathChain grabPickup1, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3, scanblue1, scanred1,backstart1;
 
     public void buildPaths() {
         /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
         scorePreload = new Path(new BezierLine(startPose, scorePose));
         scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
+        sacnPreload = new Path(new BezierLine(startPose, scanbluePose));
+        sacnPreload.setLinearHeadingInterpolation(startPose.getHeading(), scanbluePose.getHeading());
 
     /* Here is an example for Constant Interpolation
     scorePreload.setConstantInterpolation(startPose.getHeading()); */
